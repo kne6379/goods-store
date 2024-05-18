7bd0c9197380fb35b68ce6424d7a4fd089e412de
Robbie <Robbie@email.com>
2024-05-18 14:09:12 +0900

    feat: 설계 수정 #1

--- src/schemas/goods.schema.js
+++ src/schemas/goods.schema.js
@@ -1,38 +1,37 @@
 import mongoose from 'mongoose';
+import { GOODS_STATUS } from '../constants/product.constant.js';
 
-const goodsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const goodsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    manager: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(GOODS_STATUS),
+      default: GOODS_STATUS.FOR_SALE,
+    },
   },
-  description: {
-    type: String,
-    required: false,
-  },
-  manager: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  GoodsStatus: {
-    type: String,
-    default: 'FOR_SALE',
-  },
-  //상품명, 상품 설명, 담당자, 비밀번호 > req.body로 입력받음
-  //상품 id, 생성 일시, 수정 일시 > 자동생성
-  // 판매 상태는 기본 판매 중.
-});
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
 
 export default mongoose.model('goodsModel', goodsSchema);
